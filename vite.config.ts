ed6473610ae6b29fd58cd7f48d5ee8819c4dadf2
akimhac <akim.hachili@gmail.com>
2025-08-24 11:53:30 +0200

    Add basic tests and CI workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // important pour Netlify
+  base: "/",
   build: { target: "esnext" }
 });
