07e53f922a65aff32c6f34e9cdafb77506e1e14d
akimhac <akim.hachili@gmail.com>
2025-08-25 22:26:49 +0200

    fix: resolve build type errors

--- src/pages/AnnoncesPage.tsx
+++ src/pages/AnnoncesPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { sampleListings, type Listing } from "../data/sampleListings";
 import { getCurrentPosition, haversine } from "../lib/geo";
 import { ListingCard } from "../components/ListingCard";
